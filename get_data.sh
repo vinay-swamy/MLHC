be224e29fd6179783e8f77862c6abc9dc6e268f9
Vinay Swamy <swamyvinny@gmail.com>
2023-03-07 12:35:57 -0500

    asdf

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 wget -O chestXray.zip https://data.mendeley.com/public-files/datasets/rscbjbr9sj/files/f12eaf6d-6023-432f-acc9-80c9d7393433/file_downloaded
-unzip -q chestXray.zip
+unzip -qo chestXray.zip
 rm chestXray.zip
 arr=$(chest_xray/train/NORMAL/*)
 mkdir -p chest_xray/validation/NORMAL/
