17100cfa55a61b1c7a23eaff82b5cb4fd78b14a0
Vinay Swamy <swamyvinny@gmail.com>
2023-03-07 13:01:29 -0500

    are

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
-wget -O chestXray.zip https://data.mendeley.com/public-files/datasets/rscbjbr9sj/files/f12eaf6d-6023-432f-acc9-80c9d7393433/file_downloaded
-unzip -qo chestXray.zip
-rm chestXray.zip
+#wget -O chestXray.zip https://data.mendeley.com/public-files/datasets/rscbjbr9sj/files/f12eaf6d-6023-432f-acc9-80c9d7393433/file_downloaded
+#unzip -qo chestXray.zip
+#rm chestXray.zip
 arr=($(ls chest_xray/train/NORMAL/*))
 mkdir -p chest_xray/validation/NORMAL/
 for i in 0..349;
